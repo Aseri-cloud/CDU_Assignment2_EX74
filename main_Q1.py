99046cfb5d7c31842e2004a657719208e0ff1295
Aseri-cloud <s266536@students.cdu.edu.au>
2025-09-02 11:48:43 +0930

    Update main_Q1.py

--- main_Q1.py
+++ main_Q1.py
@@ -9,4 +9,123 @@
 
 #Decryption must be the exact inverse of the above.
 
-##**CODE BELOW**
+##**CODE BELOW** - Started the code for the first question - nearly works just needs a couple of edits and put into a function
+
+
+f = open("raw_text.txt", 'r')
+
+shift1 = int(input("enter shift value 1: "))
+shift2 = int(input("enter shift value 2: "))
+shift_lam = shift1 * shift2
+shift_lnz = shift1 + shift2
+shift_uam = shift1
+shift_unz = shift2 * shift2
+shift_lam %= 13
+shift_lnz %= 13
+shift_unz %= 13
+shift_uam %= 13
+code = ''
+
+for line in f:
+  for ch in line:
+    if ch.islower():
+      #a-m LOWERCASE
+      if ord(ch) >= 97 and ord(ch) <= 109:
+        ordval = ord(ch)
+        cipherValue = ordval + shift_lam
+
+        if cipherValue > ord('m'):
+          x = cipherValue - ord('m')
+          cipherValue = ord('a') + x-1
+        code += chr(cipherValue)
+      
+      #n-z LOWERCASE
+      elif ord(ch) >= 110 and ord(ch) <= 122:
+        ordval = ord(ch)
+        cipherValue = ordval - shift_lnz
+
+        if cipherValue < ord('n'):
+          x = ord('n') - cipherValue
+          cipherValue = ord('z') - x+1
+        code += chr(cipherValue)
+      else:
+        code += ch
+    #N-Z UPPERCASE
+    elif ch.isupper():
+      if ord(ch) >= 78 and ord(ch) <= 90:
+        ordval = ord(ch)
+        cipherValue = ordval + shift_unz
+
+        if cipherValue > ord('Z'):
+          x = cipherValue - ord('Z')
+          cipherValue = ord('N') + x-1
+        code += chr(cipherValue)
+      #A-M UPPERCASE
+      if ord(ch) >= 65 and ord(ch) <= 77:
+        ordval = ord(ch)
+        cipherValue = ordval - shift_uam
+
+        if cipherValue < ord('A'):
+          x = ord('A') - cipherValue
+          cipherValue = ord('M') - x+1
+        code += chr(cipherValue)
+    else:
+      code += ch
+print("encrypted:", code)
+
+
+shift_lam = (-shift_lam) %13
+shift_lnz = (-shift_lnz) %13
+shift_uam = (-shift_uam) %13
+shift_unz = (-shift_unz) %13
+
+
+for ch in code:
+    if ch.islower():
+      #a-m LOWERCASE
+      if ord(ch) >= 97 and ord(ch) <= 109:
+        ordval = ord(ch)
+        cipherValue = ordval + shift_lam
+
+        if cipherValue > ord('m'):
+          x = cipherValue - ord('m')
+          cipherValue = ord('a') + x-1
+        code += chr(cipherValue)
+      
+      #n-z LOWERCASE
+      elif ord(ch) >= 110 and ord(ch) <= 122:
+        ordval = ord(ch)
+        cipherValue = ordval - shift_lnz
+
+        if cipherValue < ord('n'):
+          x = ord('n') - cipherValue
+          cipherValue = ord('z') - x+1
+        code += chr(cipherValue)
+      else:
+        code += ch
+    #N-Z UPPERCASE
+    elif ch.isupper():
+      if ord(ch) >= 78 and ord(ch) <= 90:
+        ordval = ord(ch)
+        cipherValue = ordval + shift_unz
+
+        if cipherValue > ord('Z'):
+          x = cipherValue - ord('Z')
+          cipherValue = ord('N') + x-1
+        code += chr(cipherValue)
+      #A-M UPPERCASE
+      if ord(ch) >= 65 and ord(ch) <= 77:
+        ordval = ord(ch)
+        cipherValue = ordval - shift_uam
+
+        if cipherValue < ord('A'):
+          x = ord('A') - cipherValue
+          cipherValue = ord('M') - x+1
+        code += chr(cipherValue)
+    else:
+      code += ch
+  
+f.close()
+print("decrypted:", code)
+    
+
