b435c86f426ff7dace894c245275d8e06d874b00
Aseri-cloud <s266536@students.cdu.edu.au>
2025-09-04 16:06:56 +0930

    Update main_Q1.py
    this is finished it works - we can optimise the code.. 
    
    dictionary for the shifting values
    somehow get the negative values to use for the decrypt shift

--- main_Q1.py
+++ main_Q1.py
@@ -12,100 +12,108 @@
 ##**CODE BELOW** - Started the code for the first question - ##Updated## works does everything we need just need to optimise - e.g turn into functions - use the same function to decode etc.
 #Maybe add the shift variables to a dict??
 
-f = open("raw_text.txt", 'r')
+text = open("raw_text.txt", 'r')
 
-shift1 = int(input("enter shift value 1: "))
-shift2 = int(input("enter shift value 2: "))
+shift1 = int(input('>>Enter SHIFT1: '))
+shift2 = int(input('>>Enter SHIFT2: '))
 
-# shifting vlaues (could we store this in a dict or tuple?)
-shift_lam = shift1 * shift2
-shift_lnz = shift1 + shift2
-shift_uam = shift1
-shift_unz = shift2 * shift2
+print(">>Encryption Proccess: ENGAGED")
 
+def encryption(string1,shift1, shift2):
+  # shifting vlaues (could we store this in a dict or tuple?)
+  shift_lam = shift1 * shift2
+  shift_lnz = shift1 + shift2
+  shift_uam = shift1
+  shift_unz = shift2 * shift2
 
-#Shift modulo 13 to account for large shfiting values - simulates the rotations and the remainder is the shift (Ex. 14 % 13 is 1 so the shift is "14" but is really "1
-# ")
-shift_lam %= 13
-shift_lnz %= 13
-shift_unz %= 13
-shift_uam %= 13
+  #Shift modulo 13 to account for large shfiting values - simulates the rotations and the remainder is the shift (Ex. 14 % 13 is 1 so the shift is "14" but is really "1
+  # ")
+  shift_lam %= 13
+  shift_lnz %= 13
+  shift_unz %= 13
+  shift_uam %= 13
+  #to loop through lnes in the text file and then through the characters in the lines
 
-#Variables for storing the encrypted and decrypted code
-code = ''
-decode = ''
+  #Variables for storing the encrypted and decrypted code
+  code = ''
 
-#to loop through lnes in the text file and then through the characters in the lines
-for line in f:
-  for ch in line:
-    if ch.islower():
-      #a-m LOWERCASE
-      if ord(ch) >= 97 and ord(ch) <= 109:
-        ordval = ord(ch)
-        #shifting based on the shift conditions set previously
-        cipherValue = ordval + shift_lam
-        #this is the wrap around the half alphabet - ex. M shift 3 m -> a ->b -> c (m=c)
-        if cipherValue > ord('m'):
-          #x is the number of shifts from 'a' is this case
-          x = cipherValue - ord('m')
-          cipherValue = ord('a') + x-1
-        #adding the character to the new  encrypted striong
-        code += chr(cipherValue)
-      
-      #n-z LOWERCASE
-      elif ord(ch) >= 110 and ord(ch) <= 122:
-        ordval = ord(ch)
-        # this is the shifting notice the - for the backward shift
-        cipherValue = ordval - shift_lnz
+  for line in string1:
+    for ch in line:
+      if ch.islower():
+        #a-m LOWERCASE
+        if ord(ch) >= 97 and ord(ch) <= 109:
+          ordval = ord(ch)
+          #the shifting of the ch
+          cipherValue = ordval + shift_lam
+          #creating the wrap around
+          if cipherValue > ord('m'):
+            x = cipherValue - ord('m')
+            cipherValue = ord('a') + x-1
+          code += chr(cipherValue)
+        
+        #n-z LOWERCASE
+        elif ord(ch) >= 110 and ord(ch) <= 122:
+          ordval = ord(ch)
+          
+          #shifting backwards with the minus
+          cipherValue = ordval - shift_lnz
+          
+          #creating the opposite wrap around for the backwards shift
+          if cipherValue < ord('n'):
+            x = ord('n') - cipherValue
+            cipherValue = ord('z') - x+1
+          code += chr(cipherValue)
+        else:
+          code += ch
+      #N-Z UPPERCASE
+      elif ch.isupper():
+        if ord(ch) >= 78 and ord(ch) <= 90:
+          ordval = ord(ch)
+          cipherValue = ordval + shift_unz
 
-        if cipherValue < ord('n'):
-          x = ord('n') - cipherValue
-          cipherValue = ord('z') - x+1
-        code += chr(cipherValue)
+          if cipherValue > ord('Z'):
+            x = cipherValue - ord('Z')
+            cipherValue = ord('N') + x-1
+          code += chr(cipherValue)
+        #A-M UPPERCASE
+        if ord(ch) >= 65 and ord(ch) <= 77:
+          ordval = ord(ch)
+          cipherValue = ordval - shift_uam
+
+          if cipherValue < ord('A'):
+            x = ord('A') - cipherValue
+            cipherValue = ord('M') - x+1
+          code += chr(cipherValue)
       else:
         code += ch
-    #N-Z UPPERCASE
-    elif ch.isupper():
-      if ord(ch) >= 78 and ord(ch) <= 90:
-        ordval = ord(ch)
-        cipherValue = ordval + shift_unz
-
-        if cipherValue > ord('Z'):
-          x = cipherValue - ord('Z')
-          cipherValue = ord('N') + x-1
-        code += chr(cipherValue)
-      #A-M UPPERCASE
-      if ord(ch) >= 65 and ord(ch) <= 77:
-        ordval = ord(ch)
-        cipherValue = ordval - shift_uam
-
-        if cipherValue < ord('A'):
-          x = ord('A') - cipherValue
-          cipherValue = ord('M') - x+1
-        code += chr(cipherValue)
-    else:
-      code += ch
-
-#creating the encrypted text file and writing in the encrypted text
-with open("encrypted.txt", "w", encoding="utf-8") as f:
-    f.write(code)
     
+  return code
 
+test = encryption(text,shift1,shift2)
 
+with open("encrypted_text.txt", "w", encoding="utf-8") as f:
+    f.write(test)
+    
+print(">>Encryption process: COMPLETE")
+print(">>Encryption process: ENCRYPTION_TEXT.TXT CREATED")
 
-print('\n')
+#decryption  function // using decrypt function same code but the shifts are negative (opposite) to shift back
+def decryption(string2,shift1,shift2):
 
+  decode =""
+  shift_lam = shift1 * shift2
+  shift_lnz = shift1 + shift2
+  shift_uam = shift1
+  shift_unz = shift2 * shift2
 
 #DECRYPTION METHOD - REVERSING THE SHIFT VALUES
-shift_lam = (-shift_lam) %13
-shift_lnz = (-shift_lnz) %13
-shift_uam = (-shift_uam) %13
-shift_unz = (-shift_unz) %13
+  shift_lam = (-shift_lam) %13
+  shift_lnz = (-shift_lnz) %13
+  shift_uam = (-shift_uam) %13
+  shift_unz = (-shift_unz) %13
 
-d = open("encrypted.txt", 'r')
-
-for line in d:
-  for ch in line:
+  for line in string2:
+    for ch in line:
       if ch.islower():
         #a-m LOWERCASE
         if ord(ch) >= 97 and ord(ch) <= 109:
@@ -149,25 +157,35 @@ for line in d:
           decode += chr(cipherValue)
       else:
         decode += ch
+    
+  return decode
 
-with open("decrypted.txt", "w", encoding="utf-8") as d:
+#assigning the d variable to the encrypted_text file for the decryption
+
+d_txt = open("encrypted_text.txt", 'r')
+decode = decryption(d_txt, shift1,shift2)
+
+#UX
+print(">>Decryption Protcol: ENGAGED")
+print(">>Encryption process: DECRYPTION_TEXT.TXT CREATED")
+
+#verificatioon process
+with open("decrypted_text.txt", "w", encoding="utf-8") as d:
     d.write(decode)
-with open('decrypted.txt', 'r') as l:
+with open('decrypted_text.txt', 'r') as l:
   check_2 = l.read()
 
 with open('raw_text.txt', 'r') as e:
   check_1 = e.read()
 
+
 if check_1 == check_2:
-  print('success')
+  print('>>Decryption: SUCCESSFUL')
 else:
-  print('fail')
-
- 
-
-
-
-
+  print('>>Decryption: FAILED')
 
 f.close()
 d.close()
+
+    
+
